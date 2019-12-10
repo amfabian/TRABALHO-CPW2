64a6a68de5b42456d003d1a85840638972b38699
AlexandreFabian <xandy.fabian@gmail.com>
2019-12-10 14:28:00 -0300

    cards.js codigo limpo e comentado

--- js/card.js
+++ js/card.js
@@ -1,22 +1,17 @@
 var cardState = {
 	
 	create: function () {
+		//carrega a musica da tela.
 		this.music = game.add.audio("angelbeats1");
 		this.music.loop = true;
 		this.music.volume = .5;
 		this.music.play();
 
+		//monta o cenário, mais pratico do que utilizando a matriz do mapa. Adiciona o bg e
+		//utiliza os FORs para preencher a parte inferior com blocos
 		game.add.sprite(0, 0, 'bg_verde');
-
 		this.blocks = game.add.group();
 		this.blocks.enableBody = true;
-	
-	
-
-		//for(var i = 0; i<17;i++){
-		//	var block = this.blocks.create(i*50,1,'block');
-		//	block.body.immovable = true;
-		//}
 		for(var i = 0; i<17;i++){
 			var block = this.blocks.create(i*50,50*12,'block');
 			block.body.immovable = true;
@@ -25,94 +20,51 @@ var cardState = {
 			var block = this.blocks.create(i*50,50*11,'block');
 			block.body.immovable = true;
 		}
-		//for(var i = 0; i<12;i++){
-		//	var block = this.blocks.create(0,50*i,'block');
-		//	block.body.immovable = true;
-		//}
-		//for(var i = 0; i<12;i++){
-		//	console.log(i);
-		//	var block = this.blocks.create(50*16, 50*i,'block');
-		//	block.body.immovable = true;
-		//}
-
-
-
+		//texto de nome de tela e quantidade de cards capturados padrao nas telas MApa, card e battle.
 		var txtTitulo = game.add.text(game.world.centerX, 590, 'CARDS', { font: '20px emulogic', fill: '#fff' });
 		txtTitulo.anchor.set(0.5, 0);
-
 		this.txtCards = game.add.text(game.world.width - 15,600,'CARDS: ' + game.cards.length,{font:'15px emulogic',fill:'#fff'});
 		this.txtCards.anchor.set(1,0.5);
+		//Botao para retornar para o mapa
 		var button;
-		
 		button = game.add.button(50, 600, 'btn_mapa', this.keyS, this, 2, 1, 0);
 		button.anchor.set(0,0.5);
-
-		button.onInputOver.add(this.over, this);
-		button.onInputOut.add(this.out, this);
-		button.onInputUp.add(this.up, this);
-
-
-
+		
+		//mapa da posição no eixo X e Y de cada uma das 10 cards possiveis de se armazenar.
+		//limitado apenas pelo tamanho do vetor game.monsters
 		vetorX = [130, 260, 390, 520, 650, 130, 260, 390, 520, 650]
 		vetorY = [100, 100, 100, 100, 100, 300, 300, 300, 300, 300];
 		console.log("TAMANHO DO VETOR MONSTERS2 " + game.cards);
-
-		
+		//For adiciona os cards já caputados do array game.cards incluindo
 		game.cards.forEach(function (item, indice, array) {
 			console.log("ITEM: "+ item, indice);
+			//adiciona a imagem de cada card.
 			game.add.sprite(vetorX[indice], vetorY[indice], item);
+			//adciona o nome abaixo do card.
 			var txtpok = game.add.text(vetorX[indice], vetorY[indice]+160, item,{ font: '11px emulogic', fill: '#fff' });
-
-		
-
 		});
 
-
-
-
+		//captura de teclas para alternar entre as telas MAPA e batalaha
 		var enterKeyS = game.input.keyboard.addKey(Phaser.Keyboard.S);
 		var enterKeyD = game.input.keyboard.addKey(Phaser.Keyboard.D);
 		enterKeyS.onDown.addOnce(this.keyS, this);
 		enterKeyD.onDown.addOnce(this.keyD, this);
-
-
 	},
 
-	
-	
-
+	//alterna para a tela de mapa
 	keyS: function () {
 		this.music.stop();
-
 		console.log("PREMIDO BOTAO S")
 		game.state.start('stage1');
 	},
 
+	//alterna para a tela de batalha
 	keyD: function () {
 		this.music.stop();
-
 		console.log("PREMIDO BOTAO D")
 		game.state.start('battle');
 	},
 
 
-
-	up: function () {
-		console.log('button up', arguments);
-	},
-
-	over: function () {
-		console.log('button over');
-	},
-
-	out: function () {
-		console.log('button out');
-	},
-
-	actionOnClick: function (button) {
-
-		background.visible = !background.visible;
-
-	},
 	
 };
